93bcca1d35e69a1368d045765a5392243ea17858
jose-escobar-dev <mejiaadonay@gmail.com>
2026-01-10 09:52:22 -0500

    Add direction changes problem skeleton

---
+++ problems/control_flow/direction_changes.py
@@ -0,0 +1,12 @@
+from typing import Iterable
+
+
+def count_direction_changes(numbers: Iterable[int]) -> int:
+    """
+    Count how many times the direction changes between up and down.
+
+    Rules:
+    - Ignore non-integers.
+    - Equal values do not change direction.
+    """
+    pass
